e2c1aad07757568620d86c1169ec092ca73f030d
Ariel Mendez <arielmendez735@gmail.com>
2020-11-11 10:24:48 -0600

    Agregar funcion getType a clase Funcion

--- utils/Tablas.py
+++ utils/Tablas.py
@@ -46,6 +46,9 @@ class Funcion:
     def setType(self, typeValue):
         self.type = typeValue
 
+    def getType(self):
+        return self.type
+    
     def setName(self, name):
         self.name = name
 
